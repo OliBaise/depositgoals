680a8090ef260ceb1a990473a1f5e74624bd2c71
OliBaise <oli@olibaisedigital.com>
2025-04-25 14:56:19 +0100

    Update townsdata.js

--- townsdata.js
+++ townsdata.js
@@ -1,225 +1,226 @@
 const townsdata = {
-   "London":{
-      "2047":{
-         "price":1005623,
-         "deposit":201125,
-         "monthly":729
+   "London": {
+      "2047": {
+         "price": 1005623,
+         "deposit": 201125,
+         "monthly": 729
       },
-      "2046":{
-         "price":1026547,
-         "deposit":205309,
-         "monthly":778
+      "2046": {
+         "price": 1026547,
+         "deposit": 205309,
+         "monthly": 778
       },
-      "2045":{
-         "price":1048391,
-         "deposit":209678,
-         "monthly":832
+      "2045": {
+         "price": 1048391,
+         "deposit": 209678,
+         "monthly": 832
       },
-      "2044":{
-         "price":1071195,
-         "deposit":214239,
-         "monthly":893
+      "2044": {
+         "price": 1071195,
+         "deposit": 214239,
+         "monthly": 893
       },
-      "2043":{
-         "price":1095001,
-         "deposit":219000,
-         "monthly":961
+      "2043": {
+         "price": 1095001,
+         "deposit": 219000,
+         "monthly": 961
       },
-      "2042":{
-         "price":985578,
-         "deposit":197116,
-         "monthly":913
+      "2042": {
+         "price": 985578,
+         "deposit": 197116,
+         "monthly": 913
       },
-      "2041":{
-         "price":947404,
-         "deposit":189481,
-         "monthly":929
+      "2041": {
+         "price": 947404,
+         "deposit": 189481,
+         "monthly": 929
       },
-      "2040":{
-         "price":910834,
-         "deposit":182167,
-         "monthly":949
+      "2040": {
+         "price": 910834,
+         "deposit": 182167,
+         "monthly": 949
       },
-      "2039":{
-         "price":875801,
-         "deposit":175160,
-         "monthly":973
+      "2039": {
+         "price": 875801,
+         "deposit": 175160,
+         "monthly": 973
       },
-      "2038":{
-         "price":842240,
-         "deposit":168448,
-         "monthly":1003
+      "2038": {
+         "price": 842240,
+         "deposit": 168448,
+         "monthly": 1003
       },
-      "2037":{
-         "price":810089,
-         "deposit":162018,
-         "monthly":1039
+      "2037": {
+         "price": 810089,
+         "deposit": 162018,
+         "monthly": 1039
       },
-      "2036":{
-         "price":779287,
-         "deposit":155857,
-         "monthly":1082
+      "2036": {
+         "price": 779287,
+         "deposit": 155857,
+         "monthly": 1082
       },
-      "2035":{
-         "price":749778,
-         "deposit":149956,
-         "monthly":1136
+      "2035": {
+         "price": 749778,
+         "deposit": 149956,
+         "monthly": 1136
       },
-      "2034":{
-         "price":721508,
-         "deposit":144302,
-         "monthly":1203
+      "2034": {
+         "price": 721508,
+         "deposit": 144302,
+         "monthly": 1203
       },
-      "2033":{
-         "price":694425,
-         "deposit":138885,
-         "monthly":1286
+      "2033": {
+         "price": 694425,
+         "deposit": 138885,
+         "monthly": 1286
       },
-      "2032":{
-         "price":668481,
-         "deposit":133696,
-         "monthly":1393
+      "2032": {
+         "price": 668481,
+         "deposit": 133696,
+         "monthly": 1393
       },
-      "2031":{
-         "price":643628,
-         "deposit":128726,
-         "monthly":1532
+      "2031": {
+         "price": 643628,
+         "deposit": 128726,
+         "monthly": 1532
       },
-      "2030":{
-         "price":619822,
-         "deposit":123964,
-         "monthly":1722
+      "2030": {
+         "price": 619822,
+         "deposit": 123964,
+         "monthly": 1722
       },
-      "2029":{
-         "price":597018,
-         "deposit":119404,
-         "monthly":1990
+      "2029": {
+         "price": 597018,
+         "deposit": 119404,
+         "monthly": 1990
       },
-      "2028":{
-         "price":575174,
-         "deposit":115035,
-         "monthly":2397
+      "2028": {
+         "price": 575174,
+         "deposit": 115035,
+         "monthly": 2397
       },
-      "2027":{
-         "price":554250,
-         "deposit":110850,
-         "monthly":3079
+      "2027": {
+         "price": 554250,
+         "deposit": 110850,
+         "monthly": 3079
       },
-      "2026":{
-         "price":534205,
-         "deposit":106841,
-         "monthly":4452
+      "2026": {
+         "price": 534205,
+         "deposit": 106841,
+         "monthly": 4452
       }
    },
-   "Birmingham":{
-      "2047":{
-         "price":351742,
-         "deposit":70348,
-         "monthly":255
+   "Birmingham": {
+      "2047": {
+         "price": 351742,
+         "deposit": 70348,
+         "monthly": 255
       },
-      "2046":{
-         "price":359561,
-         "deposit":71912,
-         "monthly":272
+      "2046": {
+         "price": 359561,
+         "deposit": 71912,
+         "monthly": 272
       },
-      "2045":{
-         "price":367380,
-         "deposit":73476,
-         "monthly":292
+      "2045": {
+         "price": 367380,
+         "deposit": 73476,
+         "monthly": 292
       },
-      "2044":{
-         "price":375199,
-         "deposit":75040,
-         "monthly":313
+      "2044": {
+         "price": 375199,
+         "deposit": 75040,
+         "monthly": 313
       },
-      "2043":{
-         "price":383018,
-         "deposit":76604,
-         "monthly":336
+      "2043": {
+         "price": 383018,
+         "deposit": 76604,
+         "monthly": 336
       },
-      "2042":{
-         "price":343923,
-         "deposit":68785,
-         "monthly":318
+      "2042": {
+         "price": 343923,
+         "deposit": 68785,
+         "monthly": 318
       },
-      "2041":{
-         "price":336104,
-         "deposit":67221,
-         "monthly":330
+      "2041": {
+         "price": 336104,
+         "deposit": 67221,
+         "monthly": 330
       },
-      "2040":{
-         "price":328285,
-         "deposit":65657,
-         "monthly":342
+      "2040": {
+         "price": 328285,
+         "deposit": 65657,
+         "monthly": 342
       },
-      "2039":{
-         "price":320466,
-         "deposit":64093,
-         "monthly":356
+      "2039": {
+         "price": 320466,
+         "deposit": 64093,
+         "monthly": 356
       },
-      "2038":{
-         "price":312647,
-         "deposit":62529,
-         "monthly":372
+      "2038": {
+         "price": 312647,
+         "deposit": 62529,
+         "monthly": 372
       },
-      "2037":{
-         "price":304828,
-         "deposit":60966,
-         "monthly":391
+      "2037": {
+         "price": 304828,
+         "deposit": 60966,
+         "monthly": 391
       },
-      "2036":{
-         "price":297009,
-         "deposit":59402,
-         "monthly":413
+      "2036": {
+         "price": 297009,
+         "deposit": 59402,
+         "monthly": 413
       },
-      "2035":{
-         "price":289190,
-         "deposit":57838,
-         "monthly":438
+      "2035": {
+         "price": 289190,
+         "deposit": 57838,
+         "monthly": 438
       },
-      "2034":{
-         "price":281371,
-         "deposit":56274,
-         "monthly":469
+      "2034": {
+         "price": 281371,
+         "deposit": 56274,
+         "monthly": 469
       },
-      "2033":{
-         "price":273552,
-         "deposit":54710,
-         "monthly":507
+      "2033": {
+         "price": 273552,
+         "deposit": 54710,
+         "monthly": 507
       },
-      "2032":{
-         "price":265733,
-         "deposit":53147,
-         "monthly":554
+      "2032": {
+         "price": 265733,
+         "deposit": 53147,
+         "monthly": 554
       },
-      "2031":{
-         "price":257914,
-         "deposit":51583,
-         "monthly":614
+      "2031": {
+         "price": 257914,
+         "deposit": 51583,
+         "monthly": 614
       },
-      "2030":{
-         "price":250095,
-         "deposit":50019,
-         "monthly":695
+      "2030": {
+         "price": 250095,
+         "deposit": 50019,
+         "monthly": 695
       },
-      "2029":{
-         "price":242276,
-         "deposit":48455,
-         "monthly":808
+      "2029": {
+         "price": 242276,
+         "deposit": 48455,
+         "monthly": 808
       },
-      "2028":{
-         "price":234457,
-         "deposit":46891,
-         "monthly":977
+      "2028": {
+         "price": 234457,
+         "deposit": 46891,
+         "monthly": 977
       },
-      "2027":{
-         "price":226638,
-         "deposit":45328,
-         "monthly":1259
+      "2027": {
+         "price": 226638,
+         "deposit": 45328,
+         "monthly": 1259
       },
-      "2026":{
-         "price":218819,
-         "deposit":43764,
-         "monthly":1823
+      "2026": {
+         "price": 218819,
+         "deposit": 43764,
+         "monthly": 1823
       }
    }
+}
